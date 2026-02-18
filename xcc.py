2f782dae5f93200a2ea73ac161bc733818a4e967
goliulang <352071639@qq.com>
2026-02-18 19:46:05 +0800

    add 3 step ctl at IO Data Bus , time clock and pc jump

--- xcc.py
+++ xcc.py
@@ -12,7 +12,8 @@ op = pp | a2<<4 | a1<<4 | a0 << 8
 """
 
 opcode_dict = {
-"add" : 0b0000000000000001
+"add" : 0b0000000000000001,
+"li" : 0b0000000000000010
 }
 
 regester_dict = {
@@ -31,24 +32,46 @@ HLT = 0x0000
 
 micro = [HLT for _ in range(5)]
 
+
+i=0
+#li a1, 23
+#op = op | a1<<4 | 23<<8
+
+opcode_tmp = opcode_dict["li"] | (regester_dict["a1"]<<4) | (23 << 8)
+#binary_number = bin(opcode_tmp)
+micro[i] = opcode_tmp
+print(type(opcode_tmp), opcode_tmp)
+i=i+1
+
+
+#li a0, 88
+opcode_tmp = opcode_dict["li"] | (regester_dict["a0"]<<4) | (88 << 8)
+#binary_number = bin(opcode_tmp)
+micro[i] = opcode_tmp
+print(type(opcode_tmp), opcode_tmp)
+i=i+1
+
 #add a2, a1, a0
 #op = op | a2<<4 | a1<<8 | a0 << 12
 opcode_tmp = opcode_dict["add"] | regester_dict["a2"]<<4 | regester_dict["a1"]<<8 | regester_dict["a0"]<<12
-
 #binary_number = bin(opcode_tmp)
-micro[0] = opcode_tmp
+micro[i] = opcode_tmp
 print(type(opcode_tmp), opcode_tmp)
+i=i+1
+
 
 #add a2, a4, a3
 #op = op | a2<<4 | a3<<8 | a4 << 12
 opcode_tmp = opcode_dict["add"] | regester_dict["a2"]<<4 | regester_dict["a3"]<<8 | regester_dict["a4"]<<12
-micro[1] = opcode_tmp
+micro[i] = opcode_tmp
+i=i+1
 
 print(type(opcode_tmp), opcode_tmp)
 
 
 with open(filename, 'wb') as file:
         for value in micro:
+            print(bin(value))
             value = value.to_bytes(2, byteorder='little', signed=False)
             print(type(value), value)
             file.write(value)
